1630e68df4e4b96dc4cf7b61c582c1e1a37de9f3
Bryan McDonald <bryanmcd.gt@gmail.com>
2016-05-16 17:35:14 -0400

    Added an play / pause icon to the navbar. Updated the javascript.

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 var vid = document.getElementById("bgvid"),
 pauseButton = document.getElementById("vidpause");
+
 function vidFade() {
     vid.classList.add("stopfade");
 }
@@ -10,12 +11,32 @@ vid.addEventListener('ended', function() {
 	vidFade();
 });
 pauseButton.addEventListener("click", function() {
-    vid.classList.toggle("stopfade");
-	if (vid.paused) {
-vid.play();
-		pauseButton.innerHTML = "Pause";
-	} else {
-        vid.pause();
-        pauseButton.innerHTML = "Paused";
-	}
+    playVideoButtonClick();  
 })
+
+$(document).ready(function(){
+    $("#navBarPause").click(function(e){
+        e.preventDefault();
+        playVideoButtonClick();
+    });
+});
+
+var playVideoButtonClick = function(){
+    vid.classList.toggle("stopfade");
+    if (vid.paused)
+       playVideo();
+	else
+        pauseVideo();	
+}
+
+var playVideo = function(){
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+    $("#navBarPause i").removeClass('fa-play-circle-o').addClass('fa-pause-circle-o');    
+}
+
+var pauseVideo = function(){
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+    $("#navBarPause i").removeClass('fa-pause-circle-o').addClass('fa-play-circle-o');
+}
